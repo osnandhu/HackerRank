c2a10f7f7f90d01cf81a3cd04a9c9b88e5d771d2
Nanditha Sundararajan <os.nandhu@gmail.com>
2019-06-10 01:12:10 -0400

    commit

--- List_Comprehen.py
+++ List_Comprehen.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
 print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
-#print([[a,b,c]] for a in range(0,x+1) for b in range(0,y+1) 
+#print([[a,b,c]] for a in range(0,x+1) for b in range(0,y+1)
+# #print(hello) 
