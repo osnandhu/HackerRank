389156e34d00a285bb1f9e2264ed68acb6242143
Nanditha Sundararajan <os.nandhu@gmail.com>
2019-06-10 01:17:07 -0400

    new

--- List_Comprehen.py
+++ List_Comprehen.py
@@ -15,4 +15,4 @@ if __name__ == '__main__':
     n = int(input())
 print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
 #print([[a,b,c]] for a in range(0,x+1) for b in range(0,y+1)
-# #print(hello) 
+print("GITHUB IS CONFIGURED")
