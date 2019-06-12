80a4dfa14c2b0d1696c131eee5f15cf2309316f8
Nanditha Sundararajan <os.nandhu@gmail.com>
2019-06-11 20:21:42 -0400

    commit

--- Door_mat.py
+++ Door_mat.py
@@ -58,3 +58,9 @@ n, m = 7,21
 pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
 print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
 
+
+#pattern = [('.|.'*(2* i +1)).center(m, '-') for i in range(n//2)]
+#print('\n'.join(pattern + ['WELCOME'.center(m,'-')]+ pattern[::-1])
+
+
+
---
+++ String_Formatting.py
@@ -0,0 +1,13 @@
+"""
+pyformat: 
+"""
+def print_formatted(number):
+    #n = int(raw_input())
+    width = len("{0:b}".format(n))
+    for i in range(1,n+1):
+        print(  "{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i,               width=width))
+    
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
