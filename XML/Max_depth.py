343d139ddd250e8c08e99f954c7dae5db7aa03d5
Nanditha Sundararajan <os.nandhu@gmail.com>
2019-06-18 08:35:11 -0400

    commit!

---
+++ XML/Max_depth.py
@@ -0,0 +1,50 @@
+"""You are given a valid XML document, and you have to print the maximum level of nesting in it. Take the depth of the root as .
+
+Input Format
+
+The first line contains , the number of lines in the XML document. 
+The next  lines follow containing the XML document.
+
+Output Format
+
+Output a single line, the integer value of the maximum level of nesting in the XML document.
+
+Sample Input
+
+6
+<feed xml:lang='en'>
+    <title>HackerRank</title>
+    <subtitle lang='en'>Programming challenges</subtitle>
+    <link rel='alternate' type='text/html' href='http://hackerrank.com/'/>
+    <updated>2013-12-25T12:00:00</updated>
+</feed>
+Sample Output
+
+1
+Explanation
+
+Here, the root is a feed tag, which has depth of . 
+The tags title, subtitle, link and updated all have a depth of . 
+
+Thus, the maximum depth is .
+"""
+import xml.etree.ElementTree as etree
+
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+    level += 1
+    if level >= maxdepth:
+        maxdepth = level
+    for child in elem:
+        depth(child, level)
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
